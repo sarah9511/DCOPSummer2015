4da441318004e0f3a535a1d10466d5dce3eb6e4c
sarah9511 <sarah95@nmsu.edu>
2015-06-08 20:31:20 -0600

    Update Constraint.java

--- agentGeneration/Constraint.java
+++ agentGeneration/Constraint.java
@@ -6,6 +6,13 @@ public class Constraint{
    int arity;
    ArrayList<String> scope;
    
+   public Constraint(){
+      name = null;
+      reference = null;
+      arity = -1;
+      scope = null;
+   }//closing constructor
+   }
    public Constraint( String n, String r, int a, ArrayList<String> sc){
    
       name = n;
