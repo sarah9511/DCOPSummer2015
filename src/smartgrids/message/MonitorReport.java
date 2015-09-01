7a109d0984e82bdfedcd3da6a092a04eb0ba2dd2
Eric Frechette <frechetta93@gmail.com>
2015-08-31 21:03:42 -0600

    fixed project: agents now advance to next cycle correctly, and die after no improvement for 20 iterations. also cleaned up code

--- src/smartgrids/message/MonitorReport.java
+++ src/smartgrids/message/MonitorReport.java
@@ -1,18 +1,15 @@
 package smartgrids.message;
 
-import java.util.ArrayList;
 import java.io.Serializable;
 
 @SuppressWarnings("serial")
 public class MonitorReport implements Serializable
 {
 	public boolean active;
-	public ArrayList<Boolean> variableStatuses;   //only send whether variables are still being changed so as to avoid having one agent that knows everything?
 	
 	
-	public MonitorReport(boolean active, ArrayList<Boolean> variableStatuses)
+	public MonitorReport(boolean active)
 	{
 		this.active = active;
-		this.variableStatuses = variableStatuses;
 	}
 }
